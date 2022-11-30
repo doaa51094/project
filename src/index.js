e2b0808d9d05bf69a100b1c03c1c000b5a7470ef
doaa51094 <112436865+doaa51094@users.noreply.github.com>
2022-11-30 15:05:47 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,14 +9,6 @@ import './index.css';
 
 
 
-// const cors = require('cors');
-// const corsOptions ={
-//     origin:'http://localhost:3000', 
-//     credentials:true,            //access-control-allow-credentials:true
-//     optionSuccessStatus:200
-// }
-// app.use(cors(corsOptions));
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
